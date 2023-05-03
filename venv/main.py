cb3025ebafb958ca9e7f74e0f195382b947929ad
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-05-03 13:51:29 +0200

    create a snake 3-pc body with diff position on screen and append in to list

--- venv/main.py
+++ venv/main.py
@@ -5,6 +5,16 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+segments = []
+pos_x = 0
+pos_y = 0
+
+for segment in range(0,3):
+    segment_1 = Turtle(shape="square")
+    segment_1.color("white")
+    segment_1.setposition(x=pos_x,y=pos_y)
+    segments.append(segment_1)
+    pos_x -= 20
 
 
 
