f4b343c1632485d0cf77ce2d8e2d1d5a25a6b99d
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-05-03 13:41:01 +0200

    Import Turtle package Setup screen opt

--- venv/main.py
+++ venv/main.py
@@ -1,1 +1,12 @@
-#init
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+
+
+
+screen.exitonclick()
