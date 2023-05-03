293654a2a9e41bf3d3cbb16e97327a1e1b72c30d
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-05-03 14:26:05 +0200

    make a snake class in snake.py file make a funct move() to make a snake move from snake.py import Snake class make a snake obj with Snake class make a snake objcts move()

--- venv/main.py
+++ venv/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,31 +8,14 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-segments = []
-pos_x = 0
-pos_y = 0
-
-'''Create a 3pc snake base and append to the segments list'''
-for segment in range(0,3):
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.setposition(x=pos_x,y=pos_y)
-    segments.append(new_segment)
-    pos_x -= 20
+snake = Snake()
 
 game_is_on = True
 
-'''Make a Snake move and linked every piece and make them replace the previouse piece position'''
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) -1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    # segments[0].forward(20)
-    # segments[0].left(90)
+    snake.move()
 
 screen.exitonclick()
