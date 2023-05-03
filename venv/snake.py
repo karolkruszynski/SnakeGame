ad673f4931901a0c1a384fc3856da834a4d1be0b
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-05-03 17:43:59 +0200

    bugfix - now snake can't go opposite direction instantly.

--- venv/snake.py
+++ venv/snake.py
@@ -1,6 +1,11 @@
 from turtle import Screen, Turtle
 import time
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
 
     '''Create a 3pc snake base and append to the segments list'''
@@ -29,10 +34,14 @@ class Snake:
         self.segments[0].forward(20)
         # segments[0].left(90)
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
